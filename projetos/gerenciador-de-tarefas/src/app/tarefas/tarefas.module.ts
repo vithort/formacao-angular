bae0cc3ec887ccd0cf5f10c113270758b6ed48d5
Vithor <vithort@gmail.com>
2020-12-28 13:35:50 -0300

    Projeto 3: Gerenciador de Tarefas
    Implementando a Listagem de Tarefas - Parte 1/2

--- projetos/gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ projetos/gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
 
 @NgModule({
   declarations: [ListarTarefaComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   providers: [TarefaService],
 })
 export class TarefasModule {}
