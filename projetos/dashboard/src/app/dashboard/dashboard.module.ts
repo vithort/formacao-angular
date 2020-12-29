1d1d092efa37aa111daff2857a0d793edaf2a3d5
Vithor <vithort@gmail.com>
2020-12-29 10:29:18 -0300

    Projeto 6: Dashboard
    Criando o Componente do Dashboard

--- projetos/dashboard/src/app/dashboard/dashboard.module.ts
+++ projetos/dashboard/src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [DashboardComponent],
+  imports: [CommonModule],
+  exports: [DashboardComponent],
 })
-export class DashboardModule { }
+export class DashboardModule {}
