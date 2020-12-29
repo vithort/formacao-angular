5dac1c0fed81338a5e71764ce85d8fb07c35fea9
Vithor <vithort@gmail.com>
2020-12-29 11:30:55 -0300

    Projeto 7: Projeto Final
    Criando as Rotas do Módulo Dashboard

--- projetos/projeto-final/src/app/app-routing.module.ts
+++ projetos/projeto-final/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardRoutes } from './dashboard';
+
 export const routes: Routes = [
-  /*{
+  {
     path: '',
     redirectTo: '/dashboard',
     pathMatch: 'full',
-  },*/
+  },
+  ...DashboardRoutes,
 ];
 
 @NgModule({
