e6e89efb35a1bcfa3a8da0c6dfe28240482eb96a
Vithor <vithort@gmail.com>
2020-12-29 09:28:15 -0300

    Projeto 5: Conversor de Moedas
    Implementando o Filtro (Pipe) de Formatação de Data

--- projetos/conversor-de-moedas/src/app/conversor/pipes/data-br.pipe.spec.ts
+++ projetos/conversor-de-moedas/src/app/conversor/pipes/data-br.pipe.spec.ts
@@ -5,4 +5,9 @@ describe('DataBrPipe', () => {
     const pipe = new DataBrPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('deve formatar a data 2020-12-29 para 29/12/2020', () => {
+    const pipe = new DataBrPipe();
+    expect(pipe.transform('2020-12-29')).toEqual('29/12/2020');
+  });
 });
--- projetos/conversor-de-moedas/src/app/conversor/pipes/data-br.pipe.ts
+++ projetos/conversor-de-moedas/src/app/conversor/pipes/data-br.pipe.ts
@@ -1,12 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'dataBr'
+  name: 'dataBr',
 })
 export class DataBrPipe implements PipeTransform {
+  /**
+   * Aplica formatação para a data.
+   *
+   * @param string dataEn
+   * @return string data no formato dd/mm/yyyy
+   */
+  transform(dataEn: string): string {
+    if (!dataEn) {
+      return '';
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    const dataArr = dataEn.split('-');
+
+    if (dataArr.length !== 3) {
+      return dataEn;
+    }
+
+    return dataArr[2] + '/' + dataArr[1] + '/' + dataArr[0];
   }
-
 }
